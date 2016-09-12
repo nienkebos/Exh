e782b48f45ad833179788aebd22e02255cc6c856
nienkebos <nienkebos@live.com>
2016-09-12 23:01:10 +0200

    Added Rack Cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Exhibition.delete_all
+Artwork.delete_all
+
+Exhibition.create({
+  title: 'Impossible feasibility',
+  date: 'february - april 2017'
+  image: ''
+  description: 'Chambray selvage fap vegan, chicharrones bushwick delectus echo park brooklyn. Paleo waistcoat distillery, before they sold out pickled consequat deep v ennui hexagon pariatur art party chillwave. Do eiusmod exercitation, roof party trust fund cliche veniam tumeric you probably havent heard of them cupidatat tbh. Leggings gluten-free snackwave ullamco cillum. Cold-pressed hella cronut literally, dolore edison bulb direct trade YOLO semiotics iPhone cupidatat next level pinterest. Sint kinfolk hammock mumblecore, occupy prism quinoa vegan knausgaard tattooed affogato humblebrag forage. Wolf magna duis pop-up, adipisicing ramps tbh marfa succulents health goth copper mug seitan williamsburg laboris veniam.
+  Culpa sriracha mustache, roof party poke pug hammock slow-carb street art austin tilde duis irure gentrify. Seitan trust fund fixie, whatever occupy exercitation kickstarter photo booth pariatur adipisicing forage elit incididunt artisan franzen. Four loko ex deserunt umami austin brooklyn. Glossier ennui occaecat roof party. Raw denim ut aute, wayfarers intelligentsia chicharrones sint blue bottle. Neutra meggings sed cillum. Craft beer raw denim mollit edison bulb butcher beard meditation fap, literally fingerstache tofu.'
+  })
+Exhibition.create({
+  title: 'Francis Bacon - Retrospective',
+  date: 'november 2017 - march 2018'
+  image: ''
+  description: 'Letterpress hella bespoke, nihil lomo chambray microdosing venmo four loko id delectus man bun do 3 wolf moon slow-carb. Cronut lo-fi vape, woke pabst kogi microdosing typewriter assumenda organic nesciunt. Dolore cupidatat aute cray scenester, affogato four dollar toast austin jean shorts. Farm-to-table tofu hot chicken, poke assumenda intelligentsia art party stumptown aliqua eu next level. Offal godard disrupt, polaroid farm-to-table air plant artisan vice. Health goth occaecat vape, jianbing gastropub roof party shoreditch. Subway tile labore hashtag flannel occaecat.
+  Nulla kombucha ut, mlkshk pariatur hella officia. Mixtape farm-to-table dolore cold-pressed unicorn copper mug. Artisan you probably havent heard of them next level chicharrones cornhole pug. Copper mug 90s sriracha crucifix, consequat ad subway tile portland squid irony glossier. Normcore retro fam aliquip, air plant celiac literally bitters cray drinking vinegar intelligentsia 3 wolf moon fanny pack squid. Fingerstache locavore 3 wolf moon chambray, aliquip narwhal deserunt salvia vice vegan dolor tumblr. Crucifix assumenda yr literally gluten-free, locavore placeat.'
+})
+
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Bas van Wieringen', title: '', date: '', technique: '', image:'', description: ''})
+
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
+Artwork.create({artist: 'Francis Bacon', title: '', date: '', technique: '', image:'', description: ''})
